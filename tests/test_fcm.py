8540682c2f78aeef5697c1c397430c9246bfae44
YFY0109 <yfy0109@qq.com>
2025-03-17 09:07:08 +0800

    完成addtunnel的单元测试

--- tests/test_fcm.py
+++ tests/test_fcm.py
@@ -96,6 +96,30 @@ class TestFCMParserAddTunnel(unittest.TestCase):
         }
         output = parse_args(args)
         self.assertEqual(output, except_result)
+    
+    def test_addtunnel_2_with_options(self):
+        args = [
+            "addtunnel",
+            "8080",
+            "8081",
+            "-p",
+            "udp",
+            "-r",
+            "test_server",
+            "-n",
+            "test_tunnel",
+        ]
+        except_result = {
+            "opcode": "addtunnel",
+            "local_ip": "127.0.0.1",
+            "local_port": 8080,
+            "remote_port": 8081,
+            "remote_name": "test_server",
+            "protocol": "udp",
+            "name": "test_tunnel",
+        }
+        output=parse_args(args)
+        self.assertEqual(output, except_result)
 
     def test_addtunnel_3(self):
         args = ["addtunnel", "192.168.2.123:8080"]
@@ -111,6 +135,27 @@ class TestFCMParserAddTunnel(unittest.TestCase):
         output = parse_args(args)
         # print(output)
         self.assertEqual(output, except_result)
+        
+    def test_addtunnel_3_with_options(self):
+        args = [
+            "addtunnel",
+            "192.168.2.123:8080",
+            "-p",
+            "udp",
+            "-r",
+            "test_server",
+            "-n",
+            "test_tunnel",
+        ]
+        except_result = {
+            "opcode": "addtunnel",
+            "local_ip": "192.168.2.123",
+            "local_port": 8080,
+            "remote_port": 8080,
+            "remote_name": "test_server",
+            "protocol": "udp",
+            "name": "test_tunnel",
+        }
 
     def test_addtunnel_4(self):
         args = ["addtunnel", "192.168.2.123:8080", "8081"]
@@ -124,6 +169,30 @@ class TestFCMParserAddTunnel(unittest.TestCase):
             "name": "",
         }
         output = parse_args(args)
+        self.assertEqual(output, except_result)
+    
+    def test_addtunnel_4_with_options(self):
+        args = [
+            "addtunnel",
+            "192.168.2.123:8080",
+            "8081",
+            "-p",
+            "udp",
+            "-r",
+            "test_server",
+            "-n",
+            "test_tunnel",
+        ]
+        except_result = {
+            "opcode": "addtunnel",
+            "local_ip": "192.168.2.123",
+            "local_port": 8080,
+            "remote_port": 8081,
+            "remote_name": "test_server",
+            "protocol": "udp",
+            "name": "test_tunnel",
+        }
+        output=parse_args(args)
         self.assertEqual(output, except_result)
 
 
